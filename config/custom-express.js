22efad9288a228e7547b50da051744251aeb312c
jomas <jomasjoao@gmail.com>
2020-01-27 22:57:46 -0300

    adicionando rotas e crud de pagamentos

--- config/custom-express.js
+++ config/custom-express.js
@@ -2,12 +2,19 @@ const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
 
+
 module.exports = (app) =>{
     var app = express()
+
+
+    //como os dados podem ser recebidos
+    app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
-    
+
+    //utilizando pastas do diretório
     consign()
         .include('controllers')
+        .then('persistencia')
         .into(app)
     
     return app
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,12 +1,74 @@
+const { check, validationResult } = require('express-validator');
 
 module.exports = (app) => {
 
     app.get('/pagamentos', (req, res) => res.send('rodando na porta 3000'))
 
-    app.post('/pagamentos/pagamento', (req, res)=>{
-        const pagamento = req.body
-        console.log(pagamento)
-        res.send(pagamento)
+    app.post("/pagamentos/pagamento",function(req, res) {
+
+        //validação de resultados
+        check("forma_de_pagamento").notEmpty()
+        check("valor").notEmpty().isFloat()
+        const erros = validationResult(req)
+
+        if(!erros.isEmpty()) {
+            console.log('erros encontrados')
+            res.send(erros.array())
+        }
+
+        var pagamento = req.body;
+        console.log('processando pagamento...')
+    
+        //utilizando pastas do diretório atraves do Consign
+        var connection = app.persistencia.connectionFactory()
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection)
+
+        pagamento.status = "CRIADO";
+        pagamento.data = "20-01-01";
+    
+        pagamentoDao.salva(pagamento, (erro, result)=>{
+            if(erro)  res.status(500).send(erro)
+            console.log("pagamento criado")
+            res.location('/pagamentos/pagamento/' + result.insertId)
+            res.status(201).json(pagamento)
+        })
+      })
+
+    app.put('/pagamentos/pagamento/:id', (req, res)=>{
+
+        var pagamento = {}
+        const id = req.params.id
+
+        pagamento.id = id
+        pagamento.status = 'CONFIRMADO'
+
+        //utilizando pastas do diretório atraves do Consign
+        var connection = app.persistencia.connectionFactory()
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection)
+
+        pagamentoDao.atualiza(pagamento, (erro, result) =>{
+            if(erro) res.status(500).send(erro)
+            console.log("pagamento confirmado")
+            res.send(pagamento)
+        })
+
     })
 
+    app.delete('/pagamentos/pagamento/:id', (req, res)=>{
+        var pagamento = {}
+        const id = req.params.id
+
+        pagamento.id = id
+        pagamento.status = 'CANCELADO'
+
+        //utilizando pastas do diretório atraves do Consign
+        var connection = app.persistencia.connectionFactory()
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection)
+
+        pagamentoDao.atualiza(pagamento, (erro, result) =>{
+            if(erro) res.status(500).send(erro)
+            console.log("pagamento cancelado")
+            res.send(pagamento)
+        })
+    })
 }
